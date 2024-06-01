4bfff36a2e4d1aa6a4d1a8bd88444166234625e2
HOJUN99 <peh601@naver.com>
2024-06-01 21:26:24 +0900

    초기 커밋.

---
+++ Nine2012/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nine2012
+{
+    public partial class Form1 : Form
+    {
+        int greenCount = 9;
+        int pinkCount = 9;
+
+        bool greenTurn = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
