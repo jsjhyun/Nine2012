50b4dc1fbdcf3f96d5def0ca25bce818097b9b8b
jsjhyun <gusrnl98@naver.com>
2024-06-14 07:29:59 +0900

    feat: remove logic

--- Nine2012/Form1.cs
+++ Nine2012/Form1.cs
@@ -8,28 +8,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Nine2012
 {
     public partial class Form1 : Form
     {
-        int greenCount = 9;
-        int pinkCount = 9;
-
         bool greenTurn = true;
 
         bool greenMill = false;
         bool pinkMill = false;
+
         bool goingMove = false;
 
         PictureBox focused;
 
         char[,] board = new char[3, 8];
+        int[,] greenLoc = new int[9, 2];
+        int[,] pinkLoc = new int[9, 2];
+
+        int greenCnt = 9;
+        int pinkCnt = 9;
+        int waitGreen = 9;
+        int waitPink = 9;
 
         public Form1()
         {
             InitializeComponent();
-            
+
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 8; j++)
@@ -37,28 +43,95 @@ namespace Nine2012
                     board[i, j] = 'B';
                 }
             }
+
+            for(int i=0;i<9;i++)
+            {
+                greenLoc[i, 0] = -1;
+                greenLoc[i, 1] = -1;
+                pinkLoc[i, 0] = -1;
+                pinkLoc[i, 1] = -1;
+            }
         }
 
         private void pieceClick(object sender, MouseEventArgs e)
         {
             PictureBox piece = sender as PictureBox;
+            string color = piece.Tag.ToString();
+            string[] tmp = color.Split('-');
 
             if (greenMill)
             {
+                if (tmp[0] == "Pink")
+                {
+                    int row = pinkLoc[Int32.Parse(tmp[1]), 0];
+                    int col = pinkLoc[Int32.Parse(tmp[1]), 1];
 
+                    if (chkMill(row, col) != 1)
+                    {
+                        textBoxMessage.Text = "Impossible!";
+                    }
+                    else
+                    {
+                        piece.Visible = false;
+                        pinkCnt--;
+                        textBoxPinkCount.Text = "P : " + pinkCnt;
+
+                        if (pinkCnt < 3)
+                        {
+                            MessageBox.Show("Green Win!");
+                            Application.Restart();
+                        }
+
+                        board[row, col] = 'B';
+                        greenMill = false;
+                        textBoxMessage.Text = "Pink's Turn!";
+                    }
+                } else
+                {
+                    textBoxMessage.Text = "Select a Pink!";
+                }
             }
             else if (pinkMill)
             {
+                if (tmp[0] == "Green")
+                {
+                    int row = greenLoc[Int32.Parse(tmp[1]), 0];
+                    int col = greenLoc[Int32.Parse(tmp[1]), 1];
 
+                    if (chkMill(row, col) != 1)
+                    {
+                        textBoxMessage.Text = "Impossible!";
+                    }
+                    else
+                    {
+                        piece.Visible = false;
+                        greenCnt--;
+                        textBoxGreenCount.Text = "G : " + greenCnt;
+
+                        if (pinkCnt < 3)
+                        {
+                            MessageBox.Show("Pink Win!");
+                            Application.Restart();
+                        }
+
+                        board[row, col] = 'B';
+                        pinkMill = false;
+                        textBoxMessage.Text = "Green's Turn!";
+                    }
+                }
+                else
+                {
+                    textBoxMessage.Text = "Select a Green!";
+                }
             }
             else
             {
-                if (greenTurn && piece.Tag.ToString() == "Green")
+                if (greenTurn && tmp[0] == "Green")
                 {
                     focused = piece;
                     goingMove = true;
                 }
-                else if (!greenTurn && piece.Tag.ToString() == "Pink")
+                else if (!greenTurn && tmp[0] == "Pink")
                 {
                     focused = piece;
                     goingMove = true;
@@ -69,27 +142,217 @@ namespace Nine2012
         private void blankClick(object sender, MouseEventArgs e)
         {
             PictureBox blank = sender as PictureBox;
-
-            if (goingMove)
+            try
             {
-                focused.Location = blank.Location;
-                goingMove = false;
+                string color = focused.Tag.ToString();
+                string[] tmp = color.Split('-');
 
-                int row = blank.Tag.ToString()[0] - '0';
-                int col = blank.Tag.ToString()[1] - '0';
-
-                if (focused.Tag.ToString() == "Green")
+                if (goingMove)
                 {
-                    board[row, col] = 'G';
-                }
-                else if (focused.Tag.ToString() == "Pink")
-                {
-                    board[row, col] = 'P';
-                }
+                    goingMove = false;
 
-                focused = null;
-                greenTurn = !greenTurn;
+                    int row = blank.Tag.ToString()[0] - '0';
+                    int col = blank.Tag.ToString()[1] - '0';
+
+                    if (tmp[0] == "Green")
+                    {
+                        int oldRow = greenLoc[Int32.Parse(tmp[1]), 0];
+                        int oldCol = greenLoc[Int32.Parse(tmp[1]), 1];
+
+                        if (oldRow != -1 && oldCol != -1)
+                        {
+                            if (!chkToGo(oldRow, oldCol, row, col)) return;
+                            board[oldRow, oldCol] = 'B';
+                        }
+                        else waitGreen--;
+
+                        focused.Location = blank.Location;
+
+                        greenLoc[Int32.Parse(tmp[1]), 0] = row;
+                        greenLoc[Int32.Parse(tmp[1]), 1] = col;
+                        board[row, col] = 'G';
+
+                        if (chkMill(row, col) == 0) greenMill = true;
+
+                        if (greenMill)
+                        {
+                            textBoxMessage.Text = "Green's Mill!";
+                        }
+                        else
+                        {
+                            if(chkWin())
+                            {
+                                MessageBox.Show("Green Win!");
+                                Application.Restart();
+                            }
+                            else textBoxMessage.Text = "Pink's Turn!";
+                        }
+                    }
+                    else if (tmp[0] == "Pink")
+                    {
+                        board[row, col] = 'P';
+                        int oldRow = pinkLoc[Int32.Parse(tmp[1]), 0];
+                        int oldCol = pinkLoc[Int32.Parse(tmp[1]), 1];
+
+                        if (oldRow != -1 && oldCol != -1)
+                        {
+                            if (!chkToGo(oldRow, oldCol, row, col)) return;
+                            board[oldRow, oldCol] = 'B';
+                        }
+                        else waitPink--;
+
+                        focused.Location = blank.Location;
+
+                        pinkLoc[Int32.Parse(tmp[1]), 0] = row;
+                        pinkLoc[Int32.Parse(tmp[1]), 1] = col;
+
+                        if (chkMill(row, col) == 0) pinkMill = true;
+
+                        if (pinkMill)
+                        {
+                            textBoxMessage.Text = "Pink's Mill!";
+                        }
+                        else
+                        {
+                            if (chkWin())
+                            {
+                                MessageBox.Show("Pink Win!");
+                                Application.Restart();
+                            }
+                            else textBoxMessage.Text = "Green's Turn!";
+                        }
+                    }
+
+                    greenTurn = !greenTurn;
+                }
+            } catch (Exception ex)
+            {
+
             }
+        }
+
+        private int chkMill(int row, int col)
+        {
+            if (row < 0 || col < 0 || row > 2 || col > 7) return -1;
+            char c = 'G';
+            if(!greenTurn)
+            {
+                c = 'P';
+            }
+
+            if (col == 0)
+            {
+                if (board[row, 1] == c && board[row, 2] == c) return 0;
+                if (board[row, 6] == c && board[row, 7] == c) return 0;
+                return 1;
+            }
+            else if (col == 1)
+            {
+                if (board[row, 0] == c && board[row, 2] == c) return 0;
+                if (board[0, 1] == c && board[1, 1] == c && board[2, 1] == c) return 0;
+                return 1;
+            }
+            else if (col == 2)
+            {
+                if (board[row, 0] == c && board[row, 1] == c) return 0;
+                if (board[row, 3] == c && board[row, 4] == c) return 0;
+                return 1;
+            }
+            else if (col == 3)
+            {
+                if (board[row, 4] == c && board[row, 2] == c) return 0;
+                if (board[0, 3] == c && board[1, 3] == c && board[2, 3] == c) return 0;
+                return 1;
+            }
+            else if (col == 4)
+            {
+                if (board[row, 2] == c && board[row, 3] == c) return 0;
+                if (board[row, 5] == c && board[row, 6] == c) return 0;
+                return 1;
+            }
+            else if (col == 5)
+            {
+                if (board[row, 4] == c && board[row, 6] == c) return 0;
+                if (board[0, 5] == c && board[1, 5] == c && board[2, 5] == c) return 0;
+                return 1;
+            }
+            else if (col == 6)
+            {
+                if (board[row, 4] == c && board[row, 5] == c) return 0;
+                if (board[row, 0] == c && board[row, 7] == c) return 0;
+                return 1;
+            }
+            else if (col == 7)
+            {
+                if (board[row, 6] == c && board[row, 0] == c) return 0;
+                if (board[0, 7] == c && board[1, 7] == c && board[2, 7] == c) return 0;
+                return 1;
+            }
+            else return -1;
+        }
+
+        private bool chkWin()
+        {
+            if(greenTurn)
+            {
+                if (waitPink != 0) return false;
+                for(int i=0; i<9; i++)
+                {
+                    if (board[pinkLoc[i, 0], pinkLoc[i, 1]] != 'P') continue;
+                    for(int j=0; j<3; j++)
+                    {
+                        for(int k=0; k<8; k++)
+                        {
+                            if (board[j, k] != 'B') continue;
+                            if (chkToGo(pinkLoc[i, 0], pinkLoc[i, 1], j, k)) return false;
+                        }
+                    }
+                }
+                return true;
+            } else
+            {
+                if (waitGreen != 0) return false;
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[greenLoc[i, 0], greenLoc[i, 1]] != 'G') continue;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        for (int k = 0; k < 8; k++)
+                        {
+                            if (board[j, k] != 'B') continue;
+                            if (chkToGo(greenLoc[i, 0], greenLoc[i, 1], j, k)) return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+        private bool chkToGo(int oldRow, int oldCol, int row, int col)
+        {
+            if((oldCol == 0 && col == 7) || (oldCol == 7 && col == 0))
+            {
+                if (row == oldRow) return true;
+                else return false;
+            }
+
+            int rowSub = oldRow - row;
+            int colSub = oldCol - col;
+
+            if(rowSub == 1 || rowSub == -1)
+            {
+                if(oldCol != col) return false;
+                if (col == 0 || col == 2 || col == 4 || col == 6) return false;
+                return true;
+            }
+
+            if(colSub == 1 || colSub == -1)
+            {
+                if (row == oldRow) return true;
+                else return false;
+            }
+
+            return false;
         }
     }
 }
